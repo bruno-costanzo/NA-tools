9c7c592e5984ed01ab9c45ad3b441ec1144e92b1
bruno-costanzo <dev.bcostanzo@gmail.com>
2021-04-23 18:30:54 -0300

    Some advance in the first app. Regex issue.

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ const tacoutput = document.querySelector("#tac-output");
 
 tacinput.addEventListener("change", () => {
   tacoutput.children.tactitle.innerText = findTitle(tacinput.value);
-  tacoutput.children.tacarticle.innerText = tacinput.value;
+  tacoutput.children.tacarticle.innerText = findText(tacinput.value);
 });
 
 function findTitle(text) {
-  return /--(.*)--/g.text;
+  console.log(text);
+  return /-{2}.*-{2}/g.exec(text).toString();
+}
+
+function findText(text) {
+  return /^(-{2}.*-{2})/g.exec(text).toString();
 }
